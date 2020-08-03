fa4c5e244c4dab4aa08c8ac2344e122089ff1f0b
kangyu <pohkangyu@gmail.com>
2020-08-04 01:55:20 +0800

    Audio error - try audio changes

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -2,3 +2,12 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/anbox_x86_64.mk \
 	$(LOCAL_DIR)/anbox_armv7a_neon.mk \
 	$(LOCAL_DIR)/anbox_arm64.mk
+
+
+COMMON_LUNCH_CHOICES := \
+   anbox_x86_64-userdebug \
+   anbox_x86_64-user \
+   anbox_armv7a_neon-userdebug \
+   anbox_armv7a_neon-user \
+   anbox_arm64-userdebug \
+   anbox_arm64-user
--- products/anbox.mk
+++ products/anbox.mk
@@ -17,6 +17,8 @@
 PRODUCT_PACKAGES += \
 	egl.cfg \
 	gralloc.goldfish \
+    gralloc.goldfish.default \
+    gralloc.ranchu \
 	libGLESv1_CM_emulation \
 	lib_renderControl_enc \
 	libEGL_emulation \
@@ -31,16 +33,35 @@ PRODUCT_PACKAGES += \
 	camera.goldfish.jpeg \
 	lights.goldfish \
 	gps.goldfish \
+    gps.ranchu \
 	fingerprint.goldfish \
-	sensors.goldfish \
+ 	sensors.ranchu\
 	audio.primary.goldfish \
+    audio.primary.goldfish_legacy \
 	vibrator.goldfish \
 	power.goldfish \
 	fingerprintd
 
+#HAL
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio@4.0-impl:32 \
+    android.hardware.audio.effect@4.0-impl:32 \
+    android.hardware.health@2.0-service.goldfish \
+    audio.r_submix.default \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.effect@4.0-impl \
+    android.hardware.keymaster@3.0-service \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.power@1.1-service.ranchu \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
+    android.hardware.gnss@1.0-service \
+    android.hardware.gnss@1.0-impl \
+
 PRODUCT_COPY_FILES += \
 	vendor/anbox/android/fstab.goldfish:root/fstab.goldfish \
-	vendor/anbox/android/init.goldfish.rc:root/init.goldfish.rc \
+ 	vendor/anbox/android/init.goldfish.rc:system/etc/init/init.anbox.rc \
 	vendor/anbox/android/init.goldfish.sh:system/etc/init.goldfish.sh \
 	vendor/anbox/android/ueventd.goldfish.rc:root/ueventd.goldfish.rc \
 	vendor/anbox/android/media/media_profiles.xml:system/etc/media_profiles.xml \
@@ -49,7 +70,15 @@ PRODUCT_COPY_FILES += \
 	vendor/anbox/android/media/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
 	vendor/anbox/android/media/media_codecs_google_tv.xml:system/etc/media_codecs_google_tv.xml \
 	vendor/anbox/android/media/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-	hardware/libhardware_legacy/audio/audio_policy.conf:system/etc/audio_policy.conf
+    device/generic/goldfish/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
+    frameworks/av/services/audiopolicy/config/audio_policy_configuration_generic.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/primary_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/primary_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/surround_sound_configuration_5_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/surround_sound_configuration_5_0.xml \
+
+
 
 PRODUCT_CHARACTERISTICS := emulator
 
@@ -63,7 +92,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
 	anboxd \
 	hwcomposer.anbox \
-	AnboxAppMgr
+	AnboxAppMgr \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    hwcomposer.goldfish \
+    hwcomposer.ranchu \
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.hardware=goldfish \
@@ -98,3 +134,6 @@ $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-he
 
 PRODUCT_COPY_FILES += \
 	vendor/anbox/products/anbox.xml:system/etc/permissions/anbox.xml
+
+
+ DEVICE_MANIFEST_FILE := vendor/anbox/manifest.xml
